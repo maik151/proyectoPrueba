ae89069632a55bd388ebfc4a59036b089a980e0f
Maicol <mj4791256@gmail.com>
2024-05-16 22:35:05 -0500

    Agregar un estilos.css

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('myButton');
+    var message = document.getElementById('message');
+
+    button.addEventListener('click', function() {
+        message.textContent = '¡Hola! Has hecho clic en el botón.';
+        message.style.display = 'block';
+    });
+    document.write("holaaaa");
+    
+});
